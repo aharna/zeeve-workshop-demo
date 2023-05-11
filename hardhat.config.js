71bb7e6ec7078d8be4abad5fecc8f9d6e95d832a
Biswarghya Biswas <iambiswarghya@gmail.com>
2023-05-11 23:57:51 +0530

    updated

---
+++ hardhat.config.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+require("@nomicfoundation/hardhat-toolbox");
+
+const {ZEEVE_HTTP_URL, PRIVATE_KEY} = process.env;
+
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    polygon_mumbai: {
+      url: ZEEVE_HTTP_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  },
+}
